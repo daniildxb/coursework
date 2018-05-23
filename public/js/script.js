b8db945592254119e028da1bfc7343c1313f3ea3
Daniil Drobot <d.b.drobot@gmail.com>
2018-05-23 20:04:26 +0300

    Add error handling

--- public/js/script.js
+++ public/js/script.js
@@ -80,62 +80,67 @@ $(document).ready(function() {
             return;
         }
 
-        let blockSize = data[0];
-        $.ajax({
-            method: 'post',
-            url: 'http://localhost:3000/block',
-            dataType: 'json',
-            data: {
-                transactions: transactions,
-                blockSize: blockSize
-            },
-            success: function(data) {
-                let bounds = data['result'][1]['data']['bounds'];
-                let numeric = data['result'][0]['data']['numerical'];
-                let greedy = data['result'][2]['data']['greedy'];
-                let boundsTime = data['result'][1]['data']['time'];
-                let numericTime = data['result'][0]['data']['time'];
-                let greedyTime = data['result'][2]['data']['time'];
-                let text = 'Результат методу гілок та границь:\n';
-                let summ = 0;
-                let size = 0;
-                for (let i = 0; i < bounds.length; i++) {
-                    text += `{${bounds[i]['length']}, ${bounds[i]['fee']}}\n`;
-                    summ += +bounds[i]['fee'];
-                    size += +bounds[i]['length'];
+        if (data[1] > 250) {
+            alert('Amount of transactions is too high to be proceeded');
+        } else {
+
+            let blockSize = data[0];
+            $.ajax({
+                method: 'post',
+                url: 'http://localhost:3000/block',
+                dataType: 'json',
+                data: {
+                    transactions: transactions,
+                    blockSize: blockSize
+                },
+                success: function(data) {
+                    let bounds = data['result'][1]['data']['bounds'];
+                    let numeric = data['result'][0]['data']['numerical'];
+                    let greedy = data['result'][2]['data']['greedy'];
+                    let boundsTime = data['result'][1]['data']['time'];
+                    let numericTime = data['result'][0]['data']['time'];
+                    let greedyTime = data['result'][2]['data']['time'];
+                    let text = 'Результат методу гілок та границь:\n';
+                    let summ = 0;
+                    let size = 0;
+                    for (let i = 0; i < bounds.length; i++) {
+                        text += `{${bounds[i]['length']}, ${bounds[i]['fee']}}\n`;
+                        summ += +bounds[i]['fee'];
+                        size += +bounds[i]['length'];
+                    }
+                    text += 'Время работы алгоритма: ' + boundsTime + 'ms\n\n';
+
+                    text += '\nРезультат методу повного перебору:\n';
+                    
+                    for (let i = 0; i < numeric.length; i++) {
+                        text += `{${numeric[i]['length']}, ${numeric[i]['fee']}}\n`;
+                    }
+                    text += 'Время работы алгоритма: ' + numericTime + 'ms\n\n';
+
+                    text += '\nРезультат жадного алгоритма:\n';
+                    for (let i = 0; i < greedy.length; i++) {
+                        text += `{${greedy[i]['length']}, ${greedy[i]['fee']}}\n`;
+                    }
+                    text += 'Время работы алгоритма: ' + greedyTime + 'ms\n\n';
+
+                    summs = 'Суммарный размер блока: ' + size + '\n' + 'Суммарная комиссия: ' + summ + '\n\n\n';
+                    text = summs + text;
+                    $('textarea#result').html(text);
+
+                    var data = {
+                        labels: [numericTime + 'ms', boundsTime + 'ms', greedyTime + 'ms'],
+                        series: [numericTime, boundsTime, greedyTime]
+                    };
+
+                    new Chartist.Pie('.ct-chart', data);
+                    $('.legend').css({'display':'block'});
+                },
+                error: function(err) {
+                    console.log(err);
+                    alert('Ошибка');
                 }
-                text += 'Время работы алгоритма: ' + boundsTime + 'ms\n\n';
-
-                text += '\nРезультат методу повного перебору:\n';
-                
-                for (let i = 0; i < numeric.length; i++) {
-                    text += `{${numeric[i]['length']}, ${numeric[i]['fee']}}\n`;
-                }
-                text += 'Время работы алгоритма: ' + numericTime + 'ms\n\n';
-
-                text += '\nРезультат жадного алгоритма:\n';
-                for (let i = 0; i < greedy.length; i++) {
-                    text += `{${greedy[i]['length']}, ${greedy[i]['fee']}}\n`;
-                }
-                text += 'Время работы алгоритма: ' + greedyTime + 'ms\n\n';
-
-                summs = 'Суммарный размер блока: ' + size + '\n' + 'Суммарная комиссия: ' + summ + '\n\n\n';
-                text = summs + text;
-                $('textarea#result').html(text);
-
-                var data = {
-                  labels: [numericTime + 'ms', boundsTime + 'ms', greedyTime + 'ms'],
-                  series: [numericTime, boundsTime, greedyTime]
-                };
-
-                new Chartist.Pie('.ct-chart', data);
-                $('.legend').css({'display':'block'});
-            },
-            error: function(err) {
-                console.log(err);
-                alert('Ошибка');
-            }
-        });
+            });
+        }
     });
 
     $('.solve-tx').click(function() {
@@ -146,62 +151,68 @@ $(document).ready(function() {
             return;
         }
 
-        let transferAmount = data[0];
-        $.ajax({
-            method: 'post',
-            url: 'http://localhost:3000/tx',
-            dataType: 'json',
-            data: {
-                txInputs,
-                transferAmount
-            },
-            success: function(data) {
-                console.log(data);
-                let bounds = data['result'][1]['data']['bounds'];
-                let boundsTime = data['result'][1]['data']['time'];
-                let text = 'Результат методу гілок та границь:\n';
-                let summ = 0;
-                let size = 0;
-                for (let i = 0; i < bounds.length; i++) {
-                    text += `{${bounds[i]['amount']}, ${bounds[i]['length']}}\n`;
-                    summ += +bounds[i]['amount'];
-                    size += +bounds[i]['length'];
+        if (data[1] > 250) {
+            alert('Amount of transactions is too high to be proceeded');
+        } else {
+
+
+            let transferAmount = data[0];
+            $.ajax({
+                method: 'post',
+                url: 'http://localhost:3000/tx',
+                dataType: 'json',
+                data: {
+                    txInputs,
+                    transferAmount
+                },
+                success: function(data) {
+                    console.log(data);
+                    let bounds = data['result'][1]['data']['bounds'];
+                    let boundsTime = data['result'][1]['data']['time'];
+                    let text = 'Результат методу гілок та границь:\n';
+                    let summ = 0;
+                    let size = 0;
+                    for (let i = 0; i < bounds.length; i++) {
+                        text += `{${bounds[i]['amount']}, ${bounds[i]['length']}}\n`;
+                        summ += +bounds[i]['amount'];
+                        size += +bounds[i]['length'];
+                    }
+                    text += 'Время работы алгоритма: ' + boundsTime + 'ms\n\n';
+
+                    let numeric = data['result'][0]['data']['numerical'];
+                    let numericTime = data['result'][0]['data']['time'];
+                    text += '\nРезультат методу повного перебору:\n';
+                    for (let i = 0; i < numeric.length; i++) {
+                        text += `{${numeric[i]['amount']}, ${numeric[i]['length']}}\n`;
+                    }
+                    text += 'Время работы алгоритма: ' + numericTime + 'ms\n\n';
+
+                    let greedy = data['result'][2]['data']['greedy'];
+                    let greedyTime = data['result'][2]['data']['time'];  
+                    text += '\nРезультат жадного алгоритма:\n';
+                    for (let i = 0; i < greedy.length; i++) {
+                        text += `{${greedy[i]['amount']}, ${greedy[i]['length']}}\n`;
+                    }
+                    text += 'Время работы алгоритма: ' + greedyTime + 'ms\n\n';
+
+                    summs = 'Суммарный размер транзакции: ' + size + '\n' + 'Суммарная размер платежа: ' + summ + '\n\n\n';
+                    text = summs + text;
+                    $('textarea#result').html(text);
+
+                    var data = {
+                    labels: [numericTime + 'ms', boundsTime + 'ms', greedyTime + 'ms'],
+                    series: [numericTime, boundsTime, greedyTime]
+                    };
+
+                    new Chartist.Pie('.ct-chart', data);
+                    $('.legend').css({'display':'block'});
+                },
+                error: function(err) {
+                    console.log(err);
+                    alert('Ошибка');
                 }
-                text += 'Время работы алгоритма: ' + boundsTime + 'ms\n\n';
-
-                let numeric = data['result'][0]['data']['numerical'];
-                let numericTime = data['result'][0]['data']['time'];
-                text += '\nРезультат методу повного перебору:\n';
-                for (let i = 0; i < numeric.length; i++) {
-                    text += `{${numeric[i]['amount']}, ${numeric[i]['length']}}\n`;
-                }
-                text += 'Время работы алгоритма: ' + numericTime + 'ms\n\n';
-
-                let greedy = data['result'][2]['data']['greedy'];
-                let greedyTime = data['result'][2]['data']['time'];  
-                text += '\nРезультат жадного алгоритма:\n';
-                for (let i = 0; i < greedy.length; i++) {
-                    text += `{${greedy[i]['amount']}, ${greedy[i]['length']}}\n`;
-                }
-                text += 'Время работы алгоритма: ' + greedyTime + 'ms\n\n';
-
-                summs = 'Суммарный размер транзакции: ' + size + '\n' + 'Суммарная размер платежа: ' + summ + '\n\n\n';
-                text = summs + text;
-                $('textarea#result').html(text);
-
-                var data = {
-                  labels: [numericTime + 'ms', boundsTime + 'ms', greedyTime + 'ms'],
-                  series: [numericTime, boundsTime, greedyTime]
-                };
-
-                new Chartist.Pie('.ct-chart', data);
-                $('.legend').css({'display':'block'});
-            },
-            error: function(err) {
-                console.log(err);
-                alert('Ошибка');
-            }
-        });
+            });
+        }
     });
 
     $('.button-download').click(function() {
@@ -251,6 +262,9 @@ $(document).ready(function() {
                     text += `{${data.transactions[i]['length']}, ${data.transactions[i]['fee']}}\n`;
                 }
                 $('textarea#data').html(text);
+            },
+            error: function(error) {
+                alert(`Error: ${error.responseJSON.message}`);
             }
         });
     });
@@ -273,6 +287,9 @@ $(document).ready(function() {
                     text += `{${data.txIn[i]['amount']}, ${data.txIn[i]['length']}}\n`;
                 }
                 $('textarea#data').html(text);  
+            },
+            error: function(error) {
+                alert(`Error: ${error.responseJSON.message}`);
             }
         });
     });
